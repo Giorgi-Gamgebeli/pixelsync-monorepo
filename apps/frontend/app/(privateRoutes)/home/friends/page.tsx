7d540ab209ad797e913547b17de16542a6da5906
Giorgi <giorgi.gamgebeli.01@gmail.com>
2026-03-30 22:59:20 +0400

    testing page requests to server with empty content

--- apps/frontend/app/(privateRoutes)/home/friends/page.tsx
+++ apps/frontend/app/(privateRoutes)/home/friends/page.tsx
@@ -1,26 +1,27 @@
-import {
-  getFriends,
-  getPendingFriendRequests,
-} from "@/app/_dataAccessLayer/userActions";
-import FriendsPage from "./FriendsPage";
+// import {
+//   getFriends,
+//   getPendingFriendRequests,
+// } from "@/app/_dataAccessLayer/userActions";
+// import FriendsPage from "./FriendsPage";
 
 async function Page() {
-  const [friendsResult, pendingResult] = await Promise.all([
-    getFriends(),
-    getPendingFriendRequests(),
-  ]);
+  // const [friendsResult, pendingResult] = await Promise.all([
+  //   getFriends(),
+  //   getPendingFriendRequests(),
+  // ]);
 
-  const friends = Array.isArray(friendsResult) ? friendsResult : [];
-  const pendingFriendRequests =
-    pendingResult && "friendRequestsToThem" in pendingResult
-      ? pendingResult
-      : undefined;
+  // const friends = Array.isArray(friendsResult) ? friendsResult : [];
+  // const pendingFriendRequests =
+  //   pendingResult && "friendRequestsToThem" in pendingResult
+  //     ? pendingResult
+  //     : undefined;
 
   return (
-    <FriendsPage
-      friends={friends}
-      pendingFriendRequests={pendingFriendRequests}
-    />
+    // <FriendsPage
+    //   friends={friends}
+    //   pendingFriendRequests={pendingFriendRequests}
+    // />
+    <>friends</>
   );
 }
 
