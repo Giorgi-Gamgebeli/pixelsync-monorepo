d294dea8d0017e6c213bb91edcdb829a3570b934
Giorgi <giorgi.gamgebeli.01@gmail.com>
2026-04-14 22:19:04 +0400

    small fix

--- apps/server/src/direct-message/direct-message.module.ts
+++ apps/server/src/direct-message/direct-message.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { DirectMessageController } from './direct-message.controller';
 import { DirectMessageService } from './direct-message.service';
 import { UsersModule } from 'src/users/users.module';
@@ -7,7 +8,7 @@ import { DirectMessageGateway } from './direct-message.gateway';
 import { PresenceModule } from 'src/presence/presence.module';
 
 @Module({
-  imports: [UsersModule, CallStateModule, PresenceModule],
+  imports: [UsersModule, CallStateModule, PresenceModule, AuthModule],
   controllers: [DirectMessageController],
   providers: [DirectMessageService, DirectMessageGateway],
   exports: [DirectMessageGateway],
